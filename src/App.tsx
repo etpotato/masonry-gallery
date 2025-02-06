400ed1885a1b0e2b8f657486e01173231557f65f
Pavel Mareev <mareev.pv@gmail.com>
2025-02-06 21:04:55 +0100

    feat: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { BrowserRouter, Route, Routes, Link } from 'react-router';
 import { ThemeProvider } from 'styled-components';
 import { Button } from './components/atoms/Button';
 import { theme } from './theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      Hello world
-      <Button>First btn</Button>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Link to="/test">First link</Link>
+              </>
+            }
+          />
+          <Route
+            path="/test"
+            element={
+              <>
+                Hello world test
+                <Button>First btn</Button>
+              </>
+            }
+          />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
